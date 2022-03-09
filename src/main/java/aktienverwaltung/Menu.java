beb4c7fdf6b2a1620116b2b852a4d080c5fc248a
Jonas# <jonas.schmidt@hebk.de>
2022-03-09 13:28:51 +0100

    register method update

--- src/main/java/aktienverwaltung/Menu.java
+++ src/main/java/aktienverwaltung/Menu.java
@@ -144,7 +144,7 @@ public class Menu {
         }    
     }
 
-    //nimmt einen String und gibt ihn als hash zurück
+    //nimmt einen String und gibt ihn gehash zurück
     private String hash(String quelle){
         StringBuilder sb = new StringBuilder();
 
